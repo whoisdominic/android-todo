00a4095a5ac317387d197c5884df5fa52f3d5aad
Dominic <dominicshajaa@icloud.com>
2023-07-05 15:04:43 -0700

    restrapped: data layer ready

---
+++ app/src/main/java/com/tokul/todo/data/Todo.kt
@@ -0,0 +1,13 @@
+package com.tokul.todo.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Todo(
+    val title: String,
+    val description: String?,
+    val isDone: Boolean,
+    @PrimaryKey
+    val id: Int? = null
+)
